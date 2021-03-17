8bc41f5e4b7433834e323f0b0ad4f509475e1b6c
esavova <43584915+esavova@users.noreply.github.com>
2021-03-17 17:58:40 -0400

    Add files via upload

--- parser/Validations.java
+++ parser/Validations.java
@@ -20,7 +20,7 @@ public static String us10(HashMap<String, Family> families, HashMap<String, Indi
             if(fam.getHusbId() != null){
                 Individual husb = individuals.get(fam.getHusbId());
                 if(husb.getBirthday().yearsSince(fam.getMarried().getJavaDate()) < 14){
-                    output += "Error: Wife with ID: "+husb.getId()+" not old enough to be married on date:"+ fam.getMarried()+ ".\n";
+                    output += "Error: Husband with ID: "+husb.getId()+" not old enough to be married on date:"+ fam.getMarried()+ ".\n";
                 }
             }
         }
@@ -28,6 +28,82 @@ public static String us10(HashMap<String, Family> families, HashMap<String, Indi
     }
     return output;
 }
-    
 
+public static String us26(HashMap<String, Family> families, HashMap<String, Individual> individuals){
+    String output = "";
+    for(String key : families.keySet()){
+        Family fam = families.get(key);
+        if(fam.getWifeId() != null && individuals.get(fam.getWifeId())==null){
+            boolean found = false;
+            for(String famId: individuals.get(fam.getWifeId()).getSpouse()){
+                if(famId.equals(key)){
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){
+                output += "Error: Wife with ID: "+fam.getWifeId()+" in Family Record not found in Individuals Record.\n";
+            }
+        }
+        if(fam.getHusbId() != null && individuals.get(fam.getHusbId())==null){
+            boolean found = false;
+            for(String famId: individuals.get(fam.getHusbId()).getSpouse()){
+                if(famId.equals(key)){
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){
+                output += "Error: Husband with ID: "+fam.getHusbId()+" in Family Record not found in Individuals Record.\n";
+            }
+        }
+        if(!fam.getChildIds().isEmpty()){
+            for(String childId : fam.getChildIds()){
+                if(individuals.get(childId)==null){
+                    boolean found = false;
+                    for(String famId: individuals.get(childId).getChild()){
+                        if(famId.equals(key)){
+                            found = true;
+                            break;
+                        }
+                    }
+                    if(!found){
+                        output += "Error: Child with ID: "+childId+" in Family Record not found in Individuals Record.\n";
+                    }
+                }
+            }    
+        }
+    }
+    for(String key : individuals.keySet()){
+        Individual person = individuals.get(key);
+        
+        if(!person.getChild().isEmpty()){
+            for(String famId : person.getChild()){
+                if(families.get(famId)==null){
+                    boolean found = false;
+                    for(String childId: families.get(famId).getChildIds()){
+                        if(childId.equals(key)){
+                            found = true;
+                            break;
+                        }
+                    }
+                    if(!found){
+                        output += "Error: Family with ID: "+famId+" in Individuals Record not found in Family Record.\n";
+                    }
+                }
+            }    
+        }
+        if(!person.getSpouse().isEmpty()){
+            for(String famId : person.getSpouse()){
+                if(families.get(famId)==null){
+                    if(!families.get(famId).getHusbId().equals(key) && !families.get(famId).getWifeId().equals(key)){
+                        output += "Error: Family with ID: "+famId+" in Individuals Record not found in Family Record.\n";
+                    }
+                }
+            }    
+        }
+      
+    }
+    return output;
+}
 }// end class Validations
