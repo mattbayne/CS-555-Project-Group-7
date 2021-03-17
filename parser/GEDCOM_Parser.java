1bb126309f4bc92f6894dad3d235a1e8c45b6851
esavova <43584915+esavova@users.noreply.github.com>
2021-03-17 17:57:57 -0400

    Update GEDCOM_Parser.java

--- parser/GEDCOM_Parser.java
+++ parser/GEDCOM_Parser.java
@@ -250,6 +250,8 @@ public class GEDCOM_Parser{
 	       //ALL VALIDATING HERE AFTER FAMS AND INDIVIDUALS HAVE BEEN FILLED:
 		//User Story 10
 			fw.write(Validations.us10(families, individuals));
+		//User Story 26
+			fw.write(Validations.us26(families, individuals));
 
 	        for(String id : indi_ids) {
 	        	fw.write(individuals.get(id).toString() + "\n");
